1fcebf9ccf470f4d47036b1cad6e45dffcff3259
desaishivam26 <kane.desai@gmail.com>
2015-02-08 14:42:09 +0530

    revert:initialize intellithermal for msm8226

--- arch/arm/mach-msm/board-8226.c
+++ arch/arm/mach-msm/board-8226.c
@@ -126,8 +126,8 @@ void __init msm8226_add_drivers(void)
 	else
 		msm_clock_init(&msm8226_clock_init_data);
 	tsens_tm_init_driver();
-<<<<<<< HEAD
 	msm_thermal_device_init();
+<<<<<<< HEAD
 =======
 #ifdef CONFIG_INTELLI_THERMAL
 	msm_thermal_init(NULL);
@@ -135,6 +135,8 @@ void __init msm8226_add_drivers(void)
 	msm_thermal_device_init(NULL);
 #endif
 >>>>>>> 8182cce... board-8226: initialize intelli_thermal engine
+=======
+>>>>>>> parent of 25b8351... initialize intellithermal for msm8226
 }
 
 void __init msm8226_init(void)
